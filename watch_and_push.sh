af24494be75bd569ab2e6ec928cbef1de923b6cc
Jordan Plows <jordan@lightapi.com>
2025-02-14 21:31:09 -0800

    dev: janitor commit

--- watch_and_push.sh
+++ watch_and_push.sh
@@ -1,1 +1,14 @@
- 
+#!/bin/bash
+
+echo "Watching for file changes. Press Ctrl+C to exit."
+
+# Watch for changes in the current directory
+inotifywait -m -e close_write --format '%w%f' . | while read MODFILE
+do
+    echo "File $MODFILE has been modified."
+    git add .
+    read -r -p "Enter commit message: " commit_message
+    git commit -m "$commit_message"
+    git push origin main
+    echo "Changes pushed to GitHub."
+done 
